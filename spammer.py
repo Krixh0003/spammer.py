414226ffbf025dcebb3949335db30633dd393da9
Krixh0003 <73477504+Krixh0003@users.noreply.github.com>
2023-04-30 17:34:37 +0530

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -10,7 +10,7 @@ async def on_message(message):
               for i in range(1000):
                   await message.channel.send("type anything u want to spam ")
 
-client.run('bot token')
+client.run('Nzc3ODY3ODEzOTcxOTUxNjY2.GmMazr.wGOB-1iTOOCYyuIdZJw1E6XiBQMeVPMU0-hWYg')
 
 #install pyautogui and discord.py before running the bot.
 #for more information contact- Krish#0003.
